365462eb009a507c6aa622e1b55c002de08a0428
misha <misha@nuland.io>
2018-08-16 11:09:20 +0300

    verify that imports from preact work

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ console.log("Hello from JavaScript!");
 // check that babel works
 var name = "Bob", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
+
+import window from 'preact';
+
+// const { h, Component, render, createElement } = window.preact;
+//
+// console.log(h);
+// console.log(Component);
+// console.log(render);
+// console.log(createElement);
+
+console.log(window);
+console.log(window.h);
+console.log(window.Component);
+console.log(window.render);
+console.log(window.createElement);
