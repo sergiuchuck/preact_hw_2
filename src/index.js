9d0697cf29c1be898811becb8b28f73cafa5f323
misha <misha@nuland.io>
2018-08-19 09:53:52 +0300

    remove files, leave only index.html, dist with bundle.js and input.json

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import window from 'preact';
-import {render_recursively} from './app';
-const json = require('../input.json');
-
-const { h, Component, render, createElement } = window;
-
-render_recursively(json);
-console.log('finish');
